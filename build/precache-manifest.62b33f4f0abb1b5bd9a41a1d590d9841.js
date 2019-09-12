80d2f20ec19292e7b7ea9dfa2fba3fcb625bbe5d
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-09-12 13:40:59 +0300

    build

---
+++ build/precache-manifest.62b33f4f0abb1b5bd9a41a1d590d9841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6aff3f90456edeb443e96b06eb0e9eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "887203afda7ec0a63697",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "887203afda7ec0a63697",
+    "url": "./static/js/main.76427c55.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
