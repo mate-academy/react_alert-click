11354c35a1f89cd9eca3b961bf7fe37e9afda122
vladyslav223 <vladyslav223@gmail.com>
2019-09-12 20:09:42 +0300

    button

---
+++ build/precache-manifest.d60fb0a315367ec73c97a2c53085e6f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fd4d2ab285859c880ab0660b410ce4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c60e3c74b3ee34ee6cd",
+    "url": "./static/css/main.6c29fb93.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "1c60e3c74b3ee34ee6cd",
+    "url": "./static/js/main.74af3e73.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
