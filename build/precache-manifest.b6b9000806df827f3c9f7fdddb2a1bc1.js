fbce26d30bcfd63c208f2b2cded1710baced6c80
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-12 13:16:50 +0300

    packege.json add homepage

---
+++ build/precache-manifest.b6b9000806df827f3c9f7fdddb2a1bc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80baec8471a7bf03f258082bbeca8b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7aa22e648fd7ca37f800",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "7aa22e648fd7ca37f800",
+    "url": "./static/js/main.ca23a5bc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
