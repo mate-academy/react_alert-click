aab089c639939435cbcf3f76cc3bf2ebe8847f95
Serhii-P <serhii.pyskun@gmail.com>
2019-09-13 15:22:49 +0300

    'Solution'

---
+++ build/precache-manifest.3c90492bfec91f34a9e2b8f5ab517098.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f978ea72d0209c7d1e4910b9d4866e0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "023081675a8c44640065",
+    "url": "./static/css/main.e6c5e88f.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "023081675a8c44640065",
+    "url": "./static/js/main.4cf54be0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
