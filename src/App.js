41fa09e1804acd06f65dd25a7931abf6ce05c0a1
Rafler <amuronyk@gmail.com>
2019-09-13 10:59:14 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const clickFunc = () => {
+    alert('You were hacked)))');
+  };
   return (
-    <div>
-
+    <div className="block">
+      <button className="positive ui button" onClick={clickFunc}>
+        Click me !
+      </button>
     </div>
   );
 }
