92f981e0d2565be24ffdf5071981bfa0d7096229
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-14 12:11:12 +0300

    Solution

---
+++ build/precache-manifest.88cfaa69f337397bbfa5eebdbd0a5588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879db02b057709fad6d6da3a5065a730",
+    "url": "./index.html"
+  },
+  {
+    "revision": "060ef0df91773e37c01b",
+    "url": "./static/css/main.e34d85e8.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "060ef0df91773e37c01b",
+    "url": "./static/js/main.54d8faca.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  handleClick = () => {
+    alert('Button was clicked!');
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="wrapper">
+        <button
+          type="button"
+          className="button"
+          onClick={this.handleClick()}
+        >
+          Click me!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
