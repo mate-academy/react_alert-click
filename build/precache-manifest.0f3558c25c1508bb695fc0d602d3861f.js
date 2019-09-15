c7c9f6c304acd458956ed1227d4ca1b225fc0fa5
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-15 12:32:59 +0300

    components

---
+++ build/precache-manifest.0f3558c25c1508bb695fc0d602d3861f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1172f8fa00992cde776fec8f82f376",
+    "url": "/react_alert-click/index.html"
+  },
+  {
+    "revision": "db467e6ccb163f4e18ca",
+    "url": "/react_alert-click/static/css/main.1af489d4.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_alert-click/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "db467e6ccb163f4e18ca",
+    "url": "/react_alert-click/static/js/main.2d0abe01.chunk.js"
+  },
+  {
+    "revision": "b1bfbc4880a25fe28b9d",
+    "url": "/react_alert-click/static/js/runtime~main.52c2e316.js"
+  }
+]);
